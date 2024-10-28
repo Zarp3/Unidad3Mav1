2b70eadd6790a37e9593ad4620601ea830f9e778
Juan Cruz Quiroga <103002182+Zarp3@users.noreply.github.com>
2024-10-27 23:11:29 -0300

    Add files via upload

---
+++ Actividad 5/main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+Sprite sprite;
+
+Texture textura;
+
+int main()
+{
+	RenderWindow ventana(VideoMode(800, 600, 32), "rotacion de un sprite");
+
+
+	if (!textura.loadFromFile("cuad_yellow.png"))
+	{
+		return 0;
+	}
+
+	;
+	sprite.setTexture(textura);
+
+	sprite.setOrigin(sprite.getTexture()->getSize().x / 2, sprite.getTexture()->getSize().y / 2);
+
+	sprite.setPosition(400, 300);
+
+	while (ventana.isOpen())
+	{
+		Event evento;
+
+		while (ventana.pollEvent(evento))
+		{
+			if (evento.type == Event::Closed)
+				ventana.close();
+		}
+
+		ventana.clear();
+
+		sprite.rotate(5);
+
+		ventana.draw(sprite);
+
+		ventana.display();
+
+	}
+	return 0;
+}
